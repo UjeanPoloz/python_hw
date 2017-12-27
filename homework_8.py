3705c9cdc9411b483207d8b96defdb246608bcd8
Ievgenpolozsc <ipoloz@snapchat.com>
2017-12-27 20:55:16 +0200

    Homework: Task 8

---
+++ homework_8.py
@@ -0,0 +1,10 @@
+full_name = 'Mark Zuckerberg'
+
+first_name = full_name[:4]
+last_name = full_name[5:]
+
+true_name = last_name + ' ' + first_name
+
+print('Default name: %s\n\n'
+      'Edited name:  %s' % (full_name, true_name))
+
