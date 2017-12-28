b2a96c971b1e393fa771195a6bc7e3ac99d7ae0d
Ievgenpolozsc <ipoloz@snapchat.com>
2017-12-28 13:01:20 +0200

    Homework: Task 7 Change var name dot to separate_str

--- homework_7.py
+++ homework_7.py
@@ -9,10 +9,10 @@ def systeam_date(now = str(datetime.datetime.now())):
 current_time, split_date = systeam_date()
 current_sec = int(current_time[-2:])
 
-dot = '.'
+separate_str = '.'
 
-date_eng = split_date[1] + dot + split_date[2] + dot + split_date[0]
-date_eu = split_date[2] + dot + split_date[1] + dot + split_date[0]
+date_eng = split_date[1] + separate_str + split_date[2] + separate_str + split_date[0]
+date_eu = split_date[2] + separate_str + split_date[1] + separate_str + split_date[0]
 
 print('Date in Europe format:   %s %s\n \n'
       'Date in American format: %s %s' % (date_eu, current_time, date_eng, current_time))
