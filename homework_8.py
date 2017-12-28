b0b24fbcbd490d0e078baf9aa466448d42e013a1
Ievgenpolozsc <ipoloz@snapchat.com>
2017-12-28 13:02:55 +0200

    Homework: Task 8 Add var separate_str = ' ' to separate first name and last name

--- homework_8.py
+++ homework_8.py
@@ -3,7 +3,9 @@ full_name = 'Mark Zuckerberg'
 first_name = full_name[:4]
 last_name = full_name[5:]
 
-true_name = last_name + ' ' + first_name
+separate_str = ' '
+
+true_name = last_name + separate_str + first_name
 
 print('Default name: %s\n\n'
       'Edited name:  %s' % (full_name, true_name))
