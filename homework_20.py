853ad52aea6c7edaf0d16ce5d1f36a527762cbcd
Ievgenpolozsc <ipoloz@snapchat.com>
2018-01-20 18:41:44 +0200

    Homework: Task 20. Add dif with even and odd

--- homework_20.py
+++ homework_20.py
@@ -1,6 +1,6 @@
 import random
 
-def diff_even_odd(num_limit, ind = 10, lower_bound = -100, upper_bound = 100):
+def diff_positive_negative(num_limit, ind = 100, lower_bound = -100, upper_bound = 100):
     negative_var = 0
     positive_var = 0
 
@@ -15,6 +15,25 @@ def diff_even_odd(num_limit, ind = 10, lower_bound = -100, upper_bound = 100):
         diff = positive_var + negative_var
 
     print(num_limit)
+
+    return diff
+
+def diff_even_odd(num_limit, ind = 100, lower_bound = 0, upper_bound = 100):
+    even = 0
+    odd = 0
+    zero = 0
+
+    for i in range(ind):
+        num_limit.append(random.randint(lower_bound, upper_bound))
+
+        if num_limit[i] % 2:
+            even += num_limit[i]
+        elif num_limit[i] == 0:
+            zero += 1
+        else:
+            odd += num_limit[i]
+
+        diff = abs(even - odd)
 
     return diff
 
