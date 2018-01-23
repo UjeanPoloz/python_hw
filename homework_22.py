38354000583c3bad302c217ed8b7588c5677b265
Ievgenpolozsc <ipoloz@snapchat.com>
2018-01-23 13:13:27 +0200

    Homework: Task 22

--- homework_22.py
+++ homework_22.py
@@ -1,29 +1,25 @@
 def group_by_surname(list_of_enrollees):
-
-    groups = [['A', 'B', 'C', 'D', 'E', 'F', 'J', 'G', 'H', 'I'],
-              ['J', ' K', 'L', 'M', 'N', 'O', 'P'],
-              ['Q', 'R', 'S', 'T'],
-              ['U', 'V', 'W', 'X', 'Y', 'Z']]
-
-    count_groups = [0] * 4
-
+    list_of_groups = [["A", "B", "C", "D", "E", "F", "J", "G", "H", "I"],
+                      ["J", "K", "L", "M", "N", "O", "P"],
+                      ["Q", "R", "S", "T"],
+                      ["U", "V", "W", "X", "Y", "Z"]]
+    count_groups = [0, 0, 0, 0]
     for i in range(len(list_of_enrollees)):
         var_i = list_of_enrollees[i].split(' ')
         letter = var_i[1][:1]
-
-        for i in range(len(groups)):
-            for j in range(len(groups[i])):
-                if letter == groups[i][j]:
+        for i in range(len(list_of_groups)):
+            for j in range(len(list_of_groups[i])):
+                if letter == list_of_groups[i][j]:
                     count_groups[i] += 1
-
-
-    return count_groups
-
-
-
-list_of_names = ['Jaz Z', 'Will Smith', 'Robby Williams', 'Ujean Poloz', 'Sabina Poloz',
-                 'Jhon Doe', 'Ivan Petrenko', 'Gabe Didih']
+    var_a, var_j, var_q, var_u = count_groups
+    return var_a, var_j, var_q, var_u
 
 
 
-print(group_by_surname(list_of_names))
+list_of_names = ["Jaz Z", "Will Smith", "Robby Williams", "Ujean Poloz", "Sabina Poloz",
+                 "Jhon Doe", "Ivan Petrenko", "Gabe Didih"]
+
+list_of_names2 = ["A A", "A B", "A C", "A D"]
+
+
+print(group_by_surname(list_of_names2))
